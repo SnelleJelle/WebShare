cdd4c6bef4799b0af1e33ae162cfb42fdf772410
Jelle Criel <SnelleJelle@users.noreply.github.com>
2016-09-20 13:30:41 +0200

    Dev razor (#1)
    * added razor nuget package
    
    * removed razor, installed liquid
    
    * added contentItem entity for templating
    
    * refactored TemplateGenrator for liquid
    
    * refactored to fit naming conventions
    
    * refactored errormessaging
    
    * implemented liquid templating in ContentListing

---
+++ Server/ContentListing/ContentItem.cs
@@ -0,0 +1,16 @@
+﻿using DotLiquid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShare.Server.ContentListing
+{    
+    class ContentItem : Drop
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+    }
+}
