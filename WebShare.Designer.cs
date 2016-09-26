48185a505c35aad74b25d7ffdf6ab90123af9aa8
TommyV <tommyyeah@hotmail.com>
2016-09-26 14:54:33 +0200

    Added to design
    GUI created for allowing/blocking clients. Optimized SharedLists form
    (groupbox, tabs)

--- WebShare.Designer.cs
+++ WebShare.Designer.cs
@@ -33,13 +33,29 @@
             this.lstSharedFolders = new System.Windows.Forms.ListBox();
             this.txtNewFolder = new System.Windows.Forms.TextBox();
             this.btnAddFolder = new System.Windows.Forms.Button();
+            this.gbSharedFolders = new System.Windows.Forms.GroupBox();
+            this.tcWebShare = new System.Windows.Forms.TabControl();
+            this.tpSharedFolders = new System.Windows.Forms.TabPage();
+            this.tcClients = new System.Windows.Forms.TabPage();
+            this.lbWhitelistedClients = new System.Windows.Forms.ListBox();
+            this.lbBlacklistedClients = new System.Windows.Forms.ListBox();
+            this.lblWhitelistedClients = new System.Windows.Forms.Label();
+            this.lblBlacklistedClients = new System.Windows.Forms.Label();
+            this.btnBlacklistClient = new System.Windows.Forms.Button();
+            this.btnWhitelistclient = new System.Windows.Forms.Button();
+            this.gbClients = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.gbSharedFolders.SuspendLayout();
+            this.tcWebShare.SuspendLayout();
+            this.tpSharedFolders.SuspendLayout();
+            this.tcClients.SuspendLayout();
+            this.gbClients.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbLogo
             // 
             this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
-            this.pbLogo.Location = new System.Drawing.Point(18, 18);
+            this.pbLogo.Location = new System.Drawing.Point(17, 27);
             this.pbLogo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pbLogo.Name = "pbLogo";
             this.pbLogo.Size = new System.Drawing.Size(309, 317);
@@ -50,15 +66,15 @@
             // 
             this.lstSharedFolders.FormattingEnabled = true;
             this.lstSharedFolders.ItemHeight = 20;
-            this.lstSharedFolders.Location = new System.Drawing.Point(336, 18);
+            this.lstSharedFolders.Location = new System.Drawing.Point(350, 27);
             this.lstSharedFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lstSharedFolders.Name = "lstSharedFolders";
-            this.lstSharedFolders.Size = new System.Drawing.Size(810, 304);
+            this.lstSharedFolders.Size = new System.Drawing.Size(798, 304);
             this.lstSharedFolders.TabIndex = 1;
             // 
             // txtNewFolder
             // 
-            this.txtNewFolder.Location = new System.Drawing.Point(18, 375);
+            this.txtNewFolder.Location = new System.Drawing.Point(17, 354);
             this.txtNewFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNewFolder.Name = "txtNewFolder";
             this.txtNewFolder.Size = new System.Drawing.Size(751, 26);
@@ -66,7 +82,7 @@
             // 
             // btnAddFolder
             // 
-            this.btnAddFolder.Location = new System.Drawing.Point(780, 372);
+            this.btnAddFolder.Location = new System.Drawing.Point(779, 351);
             this.btnAddFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAddFolder.Name = "btnAddFolder";
             this.btnAddFolder.Size = new System.Drawing.Size(112, 35);
@@ -75,23 +91,138 @@
             this.btnAddFolder.UseVisualStyleBackColor = true;
             this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
             // 
+            // gbSharedFolders
+            // 
+            this.gbSharedFolders.Controls.Add(this.pbLogo);
+            this.gbSharedFolders.Controls.Add(this.btnAddFolder);
+            this.gbSharedFolders.Controls.Add(this.lstSharedFolders);
+            this.gbSharedFolders.Controls.Add(this.txtNewFolder);
+            this.gbSharedFolders.Location = new System.Drawing.Point(6, 6);
+            this.gbSharedFolders.Name = "gbSharedFolders";
+            this.gbSharedFolders.Size = new System.Drawing.Size(1210, 448);
+            this.gbSharedFolders.TabIndex = 4;
+            this.gbSharedFolders.TabStop = false;
+            // 
+            // tcWebShare
+            // 
+            this.tcWebShare.Controls.Add(this.tpSharedFolders);
+            this.tcWebShare.Controls.Add(this.tcClients);
+            this.tcWebShare.Location = new System.Drawing.Point(12, 12);
+            this.tcWebShare.Name = "tcWebShare";
+            this.tcWebShare.SelectedIndex = 0;
+            this.tcWebShare.Size = new System.Drawing.Size(1352, 619);
+            this.tcWebShare.TabIndex = 4;
+            // 
+            // tpSharedFolders
+            // 
+            this.tpSharedFolders.Controls.Add(this.gbSharedFolders);
+            this.tpSharedFolders.Location = new System.Drawing.Point(4, 29);
+            this.tpSharedFolders.Name = "tpSharedFolders";
+            this.tpSharedFolders.Padding = new System.Windows.Forms.Padding(3);
+            this.tpSharedFolders.Size = new System.Drawing.Size(1344, 586);
+            this.tpSharedFolders.TabIndex = 0;
+            this.tpSharedFolders.Text = "Shared folders";
+            this.tpSharedFolders.UseVisualStyleBackColor = true;
+            // 
+            // tcClients
+            // 
+            this.tcClients.Controls.Add(this.gbClients);
+            this.tcClients.Location = new System.Drawing.Point(4, 29);
+            this.tcClients.Name = "tcClients";
+            this.tcClients.Padding = new System.Windows.Forms.Padding(3);
+            this.tcClients.Size = new System.Drawing.Size(1344, 586);
+            this.tcClients.TabIndex = 1;
+            this.tcClients.Text = "Clients";
+            this.tcClients.UseVisualStyleBackColor = true;
+            // 
+            // lbWhitelistedClients
+            // 
+            this.lbWhitelistedClients.FormattingEnabled = true;
+            this.lbWhitelistedClients.ItemHeight = 20;
+            this.lbWhitelistedClients.Location = new System.Drawing.Point(21, 54);
+            this.lbWhitelistedClients.Name = "lbWhitelistedClients";
+            this.lbWhitelistedClients.Size = new System.Drawing.Size(191, 244);
+            this.lbWhitelistedClients.TabIndex = 0;
+            // 
+            // lbBlacklistedClients
+            // 
+            this.lbBlacklistedClients.FormattingEnabled = true;
+            this.lbBlacklistedClients.ItemHeight = 20;
+            this.lbBlacklistedClients.Location = new System.Drawing.Point(267, 54);
+            this.lbBlacklistedClients.Name = "lbBlacklistedClients";
+            this.lbBlacklistedClients.Size = new System.Drawing.Size(191, 244);
+            this.lbBlacklistedClients.TabIndex = 1;
+            // 
+            // lblWhitelistedClients
+            // 
+            this.lblWhitelistedClients.AutoSize = true;
+            this.lblWhitelistedClients.Location = new System.Drawing.Point(17, 31);
+            this.lblWhitelistedClients.Name = "lblWhitelistedClients";
+            this.lblWhitelistedClients.Size = new System.Drawing.Size(136, 20);
+            this.lblWhitelistedClients.TabIndex = 2;
+            this.lblWhitelistedClients.Text = "Whitelisted clients";
+            // 
+            // lblBlacklistedClients
+            // 
+            this.lblBlacklistedClients.AutoSize = true;
+            this.lblBlacklistedClients.Location = new System.Drawing.Point(263, 31);
+            this.lblBlacklistedClients.Name = "lblBlacklistedClients";
+            this.lblBlacklistedClients.Size = new System.Drawing.Size(134, 20);
+            this.lblBlacklistedClients.TabIndex = 3;
+            this.lblBlacklistedClients.Text = "Blacklisted clients";
+            // 
+            // btnBlacklistClient
+            // 
+            this.btnBlacklistClient.Location = new System.Drawing.Point(21, 316);
+            this.btnBlacklistClient.Name = "btnBlacklistClient";
+            this.btnBlacklistClient.Size = new System.Drawing.Size(75, 34);
+            this.btnBlacklistClient.TabIndex = 4;
+            this.btnBlacklistClient.Text = "Block";
+            this.btnBlacklistClient.UseVisualStyleBackColor = true;
+            // 
+            // btnWhitelistclient
+            // 
+            this.btnWhitelistclient.Location = new System.Drawing.Point(267, 316);
+            this.btnWhitelistclient.Name = "btnWhitelistclient";
+            this.btnWhitelistclient.Size = new System.Drawing.Size(75, 34);
+            this.btnWhitelistclient.TabIndex = 5;
+            this.btnWhitelistclient.Text = "Allow";
+            this.btnWhitelistclient.UseVisualStyleBackColor = true;
+            // 
+            // gbClients
+            // 
+            this.gbClients.Controls.Add(this.lblWhitelistedClients);
+            this.gbClients.Controls.Add(this.btnWhitelistclient);
+            this.gbClients.Controls.Add(this.lbWhitelistedClients);
+            this.gbClients.Controls.Add(this.btnBlacklistClient);
+            this.gbClients.Controls.Add(this.lbBlacklistedClients);
+            this.gbClients.Controls.Add(this.lblBlacklistedClients);
+            this.gbClients.Location = new System.Drawing.Point(6, 6);
+            this.gbClients.Name = "gbClients";
+            this.gbClients.Size = new System.Drawing.Size(1332, 574);
+            this.gbClients.TabIndex = 6;
+            this.gbClients.TabStop = false;
+            // 
             // WebShare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1166, 443);
-            this.Controls.Add(this.btnAddFolder);
-            this.Controls.Add(this.txtNewFolder);
-            this.Controls.Add(this.lstSharedFolders);
-            this.Controls.Add(this.pbLogo);
+            this.ClientSize = new System.Drawing.Size(1376, 643);
+            this.Controls.Add(this.tcWebShare);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "WebShare";
             this.Text = "WebShare";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WebShare_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.gbSharedFolders.ResumeLayout(false);
+            this.gbSharedFolders.PerformLayout();
+            this.tcWebShare.ResumeLayout(false);
+            this.tpSharedFolders.ResumeLayout(false);
+            this.tcClients.ResumeLayout(false);
+            this.gbClients.ResumeLayout(false);
+            this.gbClients.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -101,6 +232,17 @@
         private System.Windows.Forms.ListBox lstSharedFolders;
         private System.Windows.Forms.TextBox txtNewFolder;
         private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.GroupBox gbSharedFolders;
+        private System.Windows.Forms.TabControl tcWebShare;
+        private System.Windows.Forms.TabPage tpSharedFolders;
+        private System.Windows.Forms.TabPage tcClients;
+        private System.Windows.Forms.GroupBox gbClients;
+        private System.Windows.Forms.Label lblWhitelistedClients;
+        private System.Windows.Forms.Button btnWhitelistclient;
+        private System.Windows.Forms.ListBox lbWhitelistedClients;
+        private System.Windows.Forms.Button btnBlacklistClient;
+        private System.Windows.Forms.ListBox lbBlacklistedClients;
+        private System.Windows.Forms.Label lblBlacklistedClients;
     }
 }
 
